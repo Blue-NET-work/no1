3fda1e2ebaed305cfba32be0e09b74c55aabc242
Mateusz Serwinowski <biuro@blue-net.pl>
2014-08-28 10:27:30 +0200

    formularz zapisu

---
+++ zapisy.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<!-- 
+Template Name: No1
+Version: 1.00.0
+License: Custom
+-->
+<!--[if IEMobile 7]><html class="no-js iem7 oldie"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html class="no-js ie7 oldie" lang="en"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html class="no-js ie8 oldie" lang="en"><![endif]-->
+<!--[if (IE 9)&!(IEMobile)]><html class="no-js ie9" lang="en"><![endif]-->
+<!--[if (gt IE 9)|(gt IEMobile 7)]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!-- BEGIN HEAD -->
+<head>
+    <meta charset="utf-8">
+    <title>No1</title>
+
+	<!-- http://davidbcalhoun.com/2010/viewport-metatag -->
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+
+	<!-- http://www.kylejlarson.com/blog/2012/iphone-5-web-design/ -->
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    
+	<!-- BEGIN PACE PLUGIN FILES -->
+	<script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+	<link href="assets/plugins/pace/themes/pace-theme-flash.css" rel="stylesheet" type="text/css">
+	<!-- END PACE PLUGIN FILES -->
+    
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+    
+	<!-- JavaScript at bottom except for Modernizr -->
+	<script src="assets/plugins/modernizr.custom.js"></script>
+
+</head>
+<!-- END HEAD -->
+<!-- BEGIN BODY -->
+<body class="szkola_dodatkowa">
+
+<!-- BEGIN HEADER -->
+<header>
+	<div class="container">
+		<nav class="navbar navbar-default" role="navigation">
+		  <div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#header-menu">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="fa fa-navicon"></span>
+			  </button>
+			  <a class="navbar-brand animated fadeInDown" href="szkola_jezykowa.html"><img src="assets/img/logo.png" alt=""></a>
+			</div>
+			
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse nav-background" id="header-menu">
+			  <ul class="nav navbar-nav navbar-right animated fadeInDown">
+				<li><a href="dla_dzieci.html"><i class="fa fa-send-o"></i> Dla dzieci</a></li>
+                <li><a href="dla_mlodziezy.html"><i class="fa fa-globe"></i> Dla młodzieży</a></li>
+				<li><a href="dla_firm.html"><i class="fa fa-send-o"></i> Dla dorosłych i firm</a></li>
+				<li><a href="o_nas.html"><i class="fa fa-send-o"></i> O nas</a></li>
+				<li><a href="praca.html"><i class="fa fa-send-o"></i> Praca</a></li>
+				<li><a href="kontakt.html"><i class="fa fa-envelope-o"></i> Kontakt</a></li>
+			  </ul>
+			</div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</div>
+</header>
+<!-- END HEADER -->
+
+<!-- banner -->
+<div id="banner">
+
+</div>
+<!-- ./end banner -->
+<style type="text/css">
+
+.input-group-addon.primary {
+    color: rgb(255, 255, 255);
+    background-color: rgb(50, 118, 177);
+    border-color: rgb(40, 94, 142);
+}
+.input-group-addon.success {
+    color: rgb(255, 255, 255);
+    background-color: rgb(92, 184, 92);
+    border-color: rgb(76, 174, 76);
+}
+.input-group-addon.info {
+    color: rgb(255, 255, 255);
+    background-color: rgb(57, 179, 215);
+    border-color: rgb(38, 154, 188);
+}
+.input-group-addon.warning {
+    color: rgb(255, 255, 255);
+    background-color: rgb(240, 173, 78);
+    border-color: rgb(238, 162, 54);
+}
+.input-group-addon.danger {
+    color: rgb(255, 255, 255);
+    background-color: rgb(217, 83, 79);
+    border-color: rgb(212, 63, 58);
+}
+</style>
+<!-- BEGIN CONTENT -->
+<div id="content" class="container">
+	<div class="row">
+    	<div class="col-md-12">
+        	<h1 class="animated fadeInDown">Szkoła <span class="color">językowa</span> <span class="border"></span></h1>
+        </div>
+	</div>
+    
+	<div class="row">
+    
+    	<div class="col-md-4">
+            <div class="bg1Szkola"></div>
+        	<div class="box m-b-20">
+            	<h3 class="">Zapisy na rok szkolny 2014/2015 już rozpoczęte.</h3>
+                <p>Jeśli chcesz uczyć się w szkole na poziomie i zdobywać rzetelną wiedzę oraz umiejętności językowe, przyjdź do nas.</p>
+				<p>Jeśli szukasz przyjaznej i ambitnej szkoły dla swojego dziecka, nasza oferta jest skierowana właśnie do ciebie.</p>
+				<p>Nie martw się organizacją zajęć dodatkowych swojej pociechy. Zostaw problemy związane z logistyką nam.</p>
+                <p>Odbierzemy twoje dziecko ze świetlicy szkolnej i przywieziemy na miejsce po skończonych zajęciach językowych.</p>
+            	<h3 class="text-center dowoz">DOWÓZ dzieci do szkoły GRATIS!!!</h3>
+            </div>
+        	<div class="box m-b-20 active">
+            	<h3 class="">Zapisy na rok szkolny 2014/2015 już rozpoczęte.</h3>
+                <p><em>Język obcy, taniec, sport, zajęcia muzyczne, szybkie czytanie i świetlica edukacyjna.</em></p>
+                <p>Wszystkie zajęcia w jednym miejscu, bez konieczności dowożenia dziecka w kilka różnych miejsc, bez wyścigu z czasem i możliwościami rodziców.</p>
+                <p>Przyślij nam plan lekcji swojego dziecka i listę zainteresowań, a my zorganizujemy mu czas po szkole.</p>
+                <h4>Nowa lokalizacja:</h4>
+				<p>
+                	Aleja Bzów 47<br>
+                    02-495 Warszawa-Ursus
+                </p>
+            </div>
+        	<div class="box m-b-20">
+            	<h3 class="">Dowóz dzieci do szkoły językowej Gratis!!!</h3>
+                <p>Nie musisz się już martwić jak zorganizować zajęcia dodatkowe swojego dziecka.Zrobimy to za ciebie.Zabierzemy dziecko ze szkoły, nauczymy, a potem odwieziemy z powrotem.</p>
+                <p>Jeśli zechcesz, może również zostać na naszej świetlicy edukacyjnej, gdzie odrobi lekcje pod okiem nauczyciela, nauczy się kreatywnie myśleć, szybko się uczyć  i czytać.</p>
+                <p>Jego czas zostanie wypełniony zajęciami rozwojowymi, a nie bezproduktywnym koczowaniem w wielkiej, hałaśliwej grupie do przyjazdu rodziców.</p>
+                <!--<p>Więcej informacji : ( tutaj link przenoszący na stronę świetlicy )</p>-->
+            </div>
+        </div>
+    	<div class="col-md-8 text-justify content">
+
+    		<h1>Zapisy</h1>
+
+    		<p>Aby dokonać zgłoszenia na kurs językowy, należy wypełnić formularz poniżej.</p>
+    		<p>Twoje zgłoszenie zostanie przesłane na adres e-mail szkoły.</p>
+    		<p>Skontaktujemy się  i przedstawimy ci dopasowaną do ciebie ofertę kursu.</p>
+				
+			<div class="col-md-6">	
+            
+				<?php    
+                if(isset($_POST['item'])){
+                    $item = $_POST['item'];
+                
+                    require_once('assets/plugins/phpmailer/class.phpmailer.php');
+
+                    $mail = new PHPMailer(); // defaults to using php "mail()"
+                    
+                    $body = "Imię i nazwisko: ".$item['name']."(".$item['email'].")<br>
+							Wiek: ".$item['age']."<br>
+							Telefon: ".$item['phone']."
+							Wybrany język: ".$item['language']."
+							Zajęcia: ".$item['classes']."
+							Forma zajęć: ".$item['form']."
+							Ilość godzin kursu: ".$item['time']."
+							<br><br>".$item['desc'];
+                    
+                    $mail->AddReplyTo("biuro@blue-net.pl", "Biuro Blue-NET"); // change to You adres and name
+                    $mail->SetFrom("biuro@blue-net.pl", "Biuro Blue-NET"); // change to You adres and name
+                    $mail->AddReplyTo("biuro@blue-net.pl", "Biuro Blue-NET"); // change to You adres and name
+                    
+                    $mail->AddAddress($item['email'], $item['name']);
+                    
+                    $mail->Subject    = "Zapisy na zajęcia: ".$item['name'];
+                    
+                    $mail->MsgHTML($body);
+                    
+                    if(!$mail->Send()) {
+                      echo "<div class='alert alert-danger' style='width:90%'><span class='glyphicon glyphicon-ban-circle'></span> The message has not been sent!!!<br>";
+                      echo "Mailer Error: " . $mail->ErrorInfo;
+                      echo "</div>"; 
+                    } else {echo "<div class='alert alert-success' style='width:90%'><span class='glyphicon glyphicon-ok'></span> The message has been sent.</div>";}
+                    
+                }  
+                        
+                ?>
+            			
+				<form method="post">
+					<div class="form-group">
+	        			<label for="name">Imię i nazwisko</label>
+						<div class="input-group">
+							<input type="text" class="form-control" name="item[name]" id="name" placeholder="Imię i nazwisko" required>
+							<span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
+						</div>
+					</div>
+	            	<div class="form-group">
+	        			<label for="age">Wiek</label>
+						<div class="input-group" data-validate="number">
+							<input type="text" class="form-control" name="item[age]" id="age" placeholder="Wiek" required>
+							<span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
+						</div>
+					</div>
+	    			<div class="form-group">
+	        			<label for="phone">Numer telefonu</label>
+						<div class="input-group" data-validate="phone">
+							<input type="text" class="form-control" name="item[phone]" id="phone" placeholder="123-456-789" required>
+							<span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
+						</div>
+					</div>
+					<div class="form-group">
+	        			<label for="email">Email</label>
+						<div class="input-group" data-validate="email">
+							<input type="text" class="form-control" name="item[email]" id="email" placeholder="Email" required>
+							<span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
+						</div>
+					</div>
+	                <div class="form-group">
+	            		<label for="language">Wybrany język</label>
+						<div class="input-group">
+	                        <select class="form-control" name="language" id="item[language]" placeholder="Wybrany język" required>
+	                            <option value="">Wybierz język</option>
+	                            <option value="angielski">angielski</option>
+	                            <option value="niemiecki">niemiecki</option>
+	                            <option value="hiszpański">hiszpański</option>
+	                            <option value="francuski">francuski</option>
+	                            <option value="wloski">wloski</option>
+	                            <option value="hrosyjski">rosyjski</option>
+	                            <option value="ukrainski">ukrainski</option>
+	                        </select>
+							<span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
+						</div>
+					</div>
+	                <div class="form-group">
+	            		<label for="classes">Zajęcia:</label>
+						<div class="input-group">
+	                        <select class="form-control" name="item[classes]" id="classes" placeholder="Zajęcia" >
+	                            <option value="poranne">poranne</option>
+	                            <option value="popoludniowe-wieczorowe">popołudniowe-wieczorowe</option>
+	                        </select>
+							<span class="input-group-addon info"><span class="glyphicon glyphicon-asterisk"></span></span>
+						</div>
+						<small>nie dotyczy dzieci 6-12 lat oraz młodzieży szkolnej</small>
+					</div>
+	                <div class="form-group">
+	            		<label for="form">Forma zajęć:</label>
+						<div class="input-group">
+	                        <select class="form-control" name="item[form]" id="form" placeholder="Forma zajęć" required>
+	                            <option value="grupowa">grupowa</option>
+	                            <option value="indywidualna">indywidualna</option>
+	                        </select>
+							<span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
+						</div>
+					</div>
+	                <div class="form-group">
+	            		<label for="time">Ilość godzin kursu:</label>
+						<div class="input-group">
+	                        <select class="form-control" name="item[time]" id="time" placeholder="Ilość godzin kursu" required>
+	                            <option value="120">120 - 2 x tydzień 90 minut</option>
+	                            <option value="60">60 - 1 x tydzień 90 minut</option>
+	                        </select>
+							<span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
+						</div>
+					</div>
+	                <button type="submit" class="btn btn-primary col-xs-12" disabled>Wyślij</button>
+	            </form>
+	        </div>           
+        </div>
+    </div>
+    
+</div>
+<!-- END CONTENT -->
+
+
+<!-- BEGIN COPYRIGHT -->
+<div id="copyright">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12 text-right">
+                <div class="footer">
+                    &copy; 2014 No.1 Best School, Realizacja <a href="#">DigitalZone</a>.
+                </div>
+            </div>
+		</div>
+	</div>
+</div>
+<!-- END COPYRIGHT -->
+
+
+<!-- BEGIN JAVASCRIPTS -->
+	<!--[if lt IE 9]>
+	<script src="assets/plugins/respond.min.js"></script>
+	<script src="assets/plugins/excanvas.min.js"></script> 
+	<![endif]-->
+
+<!-- base javascript -->
+<script src="assets/plugins/jquery-1.10.2.min.js" type="text/javascript"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
+<script src="assets/scripts/layout.js" type="text/javascript"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+    $('.input-group input[required], .input-group textarea[required], .input-group select[required]').on('keyup change', function() {
+		var $form = $(this).closest('form'),
+            $group = $(this).closest('.input-group'),
+			$addon = $group.find('.input-group-addon'),
+			$icon = $addon.find('span'),
+			state = false;
+            
+    	if (!$group.data('validate')) {
+			state = $(this).val() ? true : false;
+		}else if ($group.data('validate') == "email") {
+			state = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test($(this).val())
+		}else if($group.data('validate') == 'phone') {
+			state = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/.test($(this).val())
+		}else if ($group.data('validate') == "length") {
+			state = $(this).val().length >= $group.data('length') ? true : false;
+		}else if ($group.data('validate') == "number") {
+			state = !isNaN(parseFloat($(this).val())) && isFinite($(this).val());
+		}
+
+		if (state) {
+				$addon.removeClass('danger');
+				$addon.addClass('success');
+				$icon.attr('class', 'glyphicon glyphicon-ok');
+		}else{
+				$addon.removeClass('success');
+				$addon.addClass('danger');
+				$icon.attr('class', 'glyphicon glyphicon-remove');
+		}
+        
+        if ($form.find('.input-group-addon.danger').length == 0) {
+            $form.find('[type="submit"]').prop('disabled', false);
+        }else{
+            $form.find('[type="submit"]').prop('disabled', true);
+        }
+	});
+    
+    $('.input-group input[required], .input-group textarea[required], .input-group select[required]').trigger('change');
+    
+    
+});
+</script>
+<!-- END JAVASCRIPTS -->
+</body>
+<!-- END BODY -->
+</html>
